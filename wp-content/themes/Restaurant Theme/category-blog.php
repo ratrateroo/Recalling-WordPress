3a3bfb98d4879ac9050749b9791338eed26d0438
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-08 23:56:56 +0800

    tried new category

--- wp-content/themes/Restaurant Theme/category-blog.php
+++ wp-content/themes/Restaurant Theme/category-blog.php
@@ -5,8 +5,8 @@
 
         <?php get_template_part('includes/section', 'archive');?>
 
-        <!-- <?php previous_posts_link();?>
-        <?php next_posts_link();?> -->
+        <?php previous_posts_link();?>
+        <?php next_posts_link();?>
     </div>
 </div>
 </section>
