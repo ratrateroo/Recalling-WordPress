b029bc09b72565abe44064dd377383c59c237a34
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-10 23:48:40 +0800

    fixed search bug

--- wp-content/themes/Restaurant Theme/searchform.php
+++ wp-content/themes/Restaurant Theme/searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get">
+<form action="http://localhost/Recalling-WordPress/" method="get">
     <label for="search">Search</label>
     <input type="text" name="s" id="search" value="<?php the_search_query();?>" required>
     <button type="submit">Search</button>
