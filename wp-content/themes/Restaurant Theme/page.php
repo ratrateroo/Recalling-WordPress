6d01bb6c2ccbb7a4e0237164819eff6400756b05
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-10 14:53:10 +0800

    added image for pages

--- wp-content/themes/Restaurant Theme/page.php
+++ wp-content/themes/Restaurant Theme/page.php
@@ -1,11 +1,15 @@
 <?php get_header()?>
-<section class="page-wrap">
-<div class="container">
-    <div class="row">
 
-        <h1><?php the_title();?></h1>
-        <?php get_template_part('includes/section', 'content');?>
+<section class="page-wrap">
+    <div class="container">
+        <!-- <div class="row"> -->
+            <h1><?php the_title();?></h1>
+                <?php if(has_post_thumbnail()):?>
+                <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title()?>" class="img-fluid mb-3 img-thumbnail">
+                <?php endif;?>
+            <?php get_template_part('includes/section', 'content');?>
+        <!-- </div> -->
     </div>
-</div>
 </section>
+
 <?php get_footer()?>
