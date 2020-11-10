645f4887e38c1cfb5ac876714340f26fe615f742
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-11 01:57:16 +0800

    added cars custom section

--- wp-content/themes/Restaurant Theme/single-cars.php
+++ wp-content/themes/Restaurant Theme/single-cars.php
@@ -3,18 +3,24 @@
 <section class="page-wrap">
 
     <div class="container">
-    
+        <h1><?php the_title();?></h1>
+    <?php if(has_post_thumbnail()):?>
 
-            <h1><?php the_title();?></h1>
-            <?php if(has_post_thumbnail()):?>
-
-            <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title()?>" class="img-fluid mb-3 img-thumbnail">
+        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title()?>" class="img-fluid mb-3 img-thumbnail">
 
 
-            <?php endif;?>
-            <?php get_template_part('includes/section', 'blogcontent');?>
-         
-        <?php wp_link_pages();?>
+    <?php endif;?>
+        <div class="row">
+            <div class="col-lg-6">
+
+                <?php get_template_part('includes/section', 'cars');?>
+                
+                <?php wp_link_pages();?>
+            </div>
+            <div class="col-lg-6">
+                <p>Another Column</p>
+            </div>
+        </div>
     </div>
 </section>
 
