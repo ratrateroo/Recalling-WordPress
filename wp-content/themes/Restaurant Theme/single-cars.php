3039311a4ef4493afa5c9bb0b3c3f06a69ff8564
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-12 19:00:32 +0800

    added list for custom fields on car post type

--- wp-content/themes/Restaurant Theme/single-cars.php
+++ wp-content/themes/Restaurant Theme/single-cars.php
@@ -4,21 +4,28 @@
 
     <div class="container">
         <h1><?php the_title();?></h1>
-    <?php if(has_post_thumbnail()):?>
+        <?php if(has_post_thumbnail()):?>
 
-        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title()?>" class="img-fluid mb-3 img-thumbnail">
+        <img src="<?php the_post_thumbnail_url('blog-large');?>" alt="<?php the_title()?>"
+            class="img-fluid mb-3 img-thumbnail">
 
 
-    <?php endif;?>
+        <?php endif;?>
         <div class="row">
             <div class="col-lg-6">
 
                 <?php get_template_part('includes/section', 'cars');?>
-                
+
                 <?php wp_link_pages();?>
             </div>
             <div class="col-lg-6">
-                <p>Another Column</p>
+
+                <ul>
+                    <li>Color: <?php echo get_post_meta($post->ID,'Color', true);?></li>
+                    <?php if( get_post_meta($post->ID,'Registration', true)) : ?>
+                    <li>Registration: <?php echo get_post_meta($post->ID,'Registration', true);?></li>
+                    <?php endif; ?>
+                </ul>
             </div>
         </div>
     </div>
