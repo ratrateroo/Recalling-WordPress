48d47c21d6ef2668de719e24e32ca386230f5f31
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-16 23:36:44 +0800

    retained inputs after clicking search

--- wp-content/themes/Restaurant Theme/car-search.php
+++ wp-content/themes/Restaurant Theme/car-search.php
@@ -19,18 +19,20 @@ $brands = get_terms([
                 <form action="<?php echo home_url('/car-search');?>" method="GET">
                     <div class="form-group">
                         <label for="keyword">Type a keyword:</label>
-                        <input id="keyword" type="text" name="keyboard" placeholder="Type a keyword"
-                            class="form-control" />
+                        <input id="keyword" type="text" name="keyword" placeholder="Type a keyword" class="form-control"
+                            value="<?php echo isset($_GET['keyword']) ? $_GET['keyword'] : '';?>" />
                     </div>
                     <div class="form-group">
                         <label for="brand">Choose a brand</label>
                         <select id="brand" name="brand" class="form-control">
                             <option value="">Choose a brand</option>
                             <?php foreach($brands as $brand): ?>
-                            <option value="<?php echo $brand->slug;?>"><?php echo $brand->name;?></option>
+                            <option <?php if( isset($_GET['brand']) && ( $_GET['brand'] == $brand->slug) ): ?> selected
+                                <?php endif;?> value="<?php echo $brand->slug;?>"><?php echo $brand->name;?></option>
                             <?php endforeach;?>
                         </select>
                     </div>
+                    <button class="btn btn-success btn btn-block">Search</button>
                 </form>
             </div>
         </div>
