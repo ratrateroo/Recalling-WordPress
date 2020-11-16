72fa6613265610257a52a6c3cf53209ad318593a
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-16 22:17:02 +0800

    added form for search

--- wp-content/themes/Restaurant Theme/car-search.php
+++ wp-content/themes/Restaurant Theme/car-search.php
@@ -3,3 +3,24 @@
 Template Name: Car Search
 */
 ?>
+<?php get_header()?>
+
+<section class="page-wrap">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <form action="<?php echo home_url('/car-search');?>" method="GET">
+                    <div class="form-group">
+                        <label for="keyword">Type a keyword:</label>
+                        <input id="keyword" type="text" name="keyboard" placeholder="Type a keyword"
+                            class="form-control" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
+</section>
+
+<?php get_footer()?>
