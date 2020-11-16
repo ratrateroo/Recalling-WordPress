6d9d8c8508b5ea35aade023f50ee9502ba323fc7
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-16 23:08:09 +0800

    added select option for brand

--- wp-content/themes/Restaurant Theme/car-search.php
+++ wp-content/themes/Restaurant Theme/car-search.php
@@ -2,6 +2,13 @@
 /*
 Template Name: Car Search
 */
+
+$brands = get_terms([
+    'taxonomy' => 'brands',
+    'hide_empty' => false,
+]);
+
+
 ?>
 <?php get_header()?>
 
@@ -14,6 +21,15 @@ Template Name: Car Search
                         <label for="keyword">Type a keyword:</label>
                         <input id="keyword" type="text" name="keyboard" placeholder="Type a keyword"
                             class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="brand">Choose a brand</label>
+                        <select id="brand" name="brand" class="form-control">
+                            <option value="">Choose a brand</option>
+                            <?php foreach($brands as $brand): ?>
+                            <option value="<?php echo $brand->slug;?>"><?php echo $brand->name;?></option>
+                            <?php endforeach;?>
+                        </select>
                     </div>
                 </form>
             </div>
