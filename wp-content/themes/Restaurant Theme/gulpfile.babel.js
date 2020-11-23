eea0e38511648e63a0cb357756086e36a560f38d
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-23 15:13:49 +0800

    added postcss autoprefixer and sourcemaps

--- wp-content/themes/Restaurant Theme/gulpfile.babel.js
+++ wp-content/themes/Restaurant Theme/gulpfile.babel.js
@@ -3,11 +3,19 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
 const PRODUCTION = yargs.argv.prod;
+
 
 export const styles = () => {
   return src('./scss/bootstrap.scss')
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
+    .pipe(gulpif(PRODUCTION, postcss([ autoprefixer ])))
     .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
     .pipe(dest('./css'));
 }
